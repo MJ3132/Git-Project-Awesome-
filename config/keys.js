65a707c947e5fadd766a0df7e787cf7aff9bccaf
Mjay <martin_jarrin@hotmail.com>
2019-07-10 16:17:17 -0400

    removed keys.js

---
+++ config/keys.js
@@ -0,0 +1,15 @@
+module.exports = {
+    google: {
+        clientID: '715391748812-b64j240b9f1o9bfmvr02pabfscpn91qv.apps.googleusercontent.com',
+        clientSecret: 'PjcZWy-7ggrLECbRwRGWofIp'
+    },
+    mongodb:
+    {
+        dbURI: 'mongodb+srv://martyj:picmaker@cluster0-cp3mt.mongodb.net/picAwesome?retryWrites=true&w=majority'
+    },
+    session:
+    {
+        cookieKey: 'ILoveJavaScript Itis Awesome'
+    }
+};
+
